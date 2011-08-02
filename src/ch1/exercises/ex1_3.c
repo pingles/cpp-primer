770a5f31098ce0e032915c228af2b14612cc7fa7
Paul Ingles <paul@oobaloo.co.uk>
2011-08-02 23:18:15 +0100

    Add 1.3

---
+++ src/ch1/exercises/ex1_3.c
@@ -0,0 +1,10 @@
+/* Exercise 1.3: Write a program to print “Hello, World” on the standard output */
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, World" << std::endl;
+  
+  return 0;
+}
