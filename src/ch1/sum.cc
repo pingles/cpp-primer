23c86d8d63bb813adf8881d445257839096ac9f9
Paul Ingles <paul@oobaloo.co.uk>
2011-08-02 23:15:19 +0100

    Add first example from book

---
+++ src/ch1/sum.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Enter two numbers: ";
+  int v1, v2;
+
+  std::cin >> v1 >> v2;
+
+  std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+
+  return 0;
+}
